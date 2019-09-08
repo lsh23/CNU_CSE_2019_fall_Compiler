001d3976b7a6c7da1eb5552f4f6d67fb863d1e6b
lsh23 <shseoul14@gmail.com>
2019-09-08 16:18:37 +0900

    add compile()
    실질적으로 compile 작업를 수행하는 코드들 추가
    inner_run & compile_ntc 함수추가

--- src/Noo_compiler.java
+++ src/Noo_compiler.java
@@ -1,4 +1,5 @@
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -51,7 +52,88 @@ public class Noo_compiler {
             return head;                            // """"","""",""" 아닌 경우는 leaf가 아닌 inner node에 속한다.
         }
     }
+    public void compile(){
+        try {
+            fw.write("#include<stdio.h>\n" +
+                    "int main(void){\n" +
+                    "\tint r;\n");
+            compile_ntc(this.head);
+            fw.write("\treturn 0;\n" +
+                    "}");
+            fw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void compile_ntc(Node node){
+        if(node.token.equals(Noo_symbol.IFXYZ.getString())){
+            // """"" 경우 x,y,z 에 대해서 inner_run을 한뒤 x가 !=0 인지 확인하고 참이면 y 아니면 z 하도록 한다.
+            Node x = node.next;
+            inner_run(x);
+            try {
+                fw.write("\tif(r != 0) {\n\t");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Node y = x.next;
+            inner_run(y);
+            try {
+                fw.write("\t}\n\telse{\n\t");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Node z = y.next;
+            inner_run(z);
+            try {
+                fw.write("\t}\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            node = z.next;
+        }
+        if(node != null){
+            // """""" 가 아닌 경우에는 inner_run을 해주면 된다.
+            inner_run(node);
+        }
+        if(node != null && node.next !=null){
+            compile_ntc(node.next);
+            // next가 있는 경우는 next에 대해서 compile 해준다.
+        }
+    }
 
+
+
+    public void inner_run(Node node) {
+        if (node.inner_next != null) {
+            // leaf 노드에서 부터 거꾸로 올라오면서 진행
+            inner_run(node.inner_next);
+        }
+        if (node.token.equals(Noo_symbol.ZERO.getString())) {
+            try {
+                //""" 인경우 r=0
+                fw.write("\tr=0;\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (node.token.equals(Noo_symbol.ADDONE.getString())) {
+            try {
+                // "" 인 경우 r = r + 1
+                fw.write("\tr = r+1;\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (node.token.equals(Noo_symbol.PRINT.getString())) {
+            try {
+                // " 인 경우 printf("%d",r)
+                fw.write("\tprintf(\"%d\",r);\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
     public static class Node<T> {
