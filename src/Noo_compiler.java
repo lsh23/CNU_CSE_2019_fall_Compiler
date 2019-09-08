fd3f2c2353b2448c64d2874866dab06a6bac2915
lsh23 <shseoul14@gmail.com>
2019-09-08 16:16:38 +0900

    add node_generator and Noo_complier generator
    노드들을 재귀적으로 순환하면서 틀을 만들어주는 generator 함수와
    
    Noo_compiler 생성자 코드 추가

--- src/Noo_compiler.java
+++ src/Noo_compiler.java
@@ -1,4 +1,7 @@
 import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class Noo_compiler {
 
@@ -20,6 +23,36 @@ public class Noo_compiler {
             return name;
         }
     }
+
+    Noo_compiler(String input, FileWriter fw) {
+        this.input = input;
+        this.fw = fw;
+        final String[] tokens = input.split("'");
+        List tmp = Arrays.asList(tokens).subList(1, tokens.length);
+        this.head = generator(tmp.iterator());
+    }
+
+    public <T> Node generator(Iterator<T> iter) {
+        T token = iter.next();
+        if (token.equals("\"\"\"")) {
+            return new Node(token); // """ 면 leaf 노드 니까 노드 생성하고 return
+        }
+        if (token.equals("\"\"\"\"")) {
+            return new Node(token, generator(iter)); // """"면 leaf 노드 아니니까 노드 생성하고 next에 다음 node
+        }
+        if (token.equals("\"\"\"\"\"")) {
+            return new Node(token, generator(iter)); // """""면 leaf 노드 아니니까 노드 생성하고 next에 다음 node
+        } else {
+            Node head = new Node(token);
+            head.inner_next = generator(iter);
+            if (iter.hasNext()) {
+                head.next = generator(iter);
+            }
+            return head;                            // """"","""",""" 아닌 경우는 leaf가 아닌 inner node에 속한다.
+        }
+    }
+
+
 
     public static class Node<T> {
 
